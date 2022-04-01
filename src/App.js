83dfc39e3165c6031a26df1718ceb5a7ab7aabbb
jennie-edlund-nti-johanneberg <74712753+jennie-edlund-nti-johanneberg@users.noreply.github.com>
2022-04-01 18:55:16 +0200

    impoterat allt (kanske)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import SignIn from './SignIn'
+import ChatRoom from './ChatRoom'
+import firebase from 'firebase/compat/app'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+// import {getAuth} from 'firebase/auth';
+// import {getFirestore} from 'firebase/firestore';
+// import { initializeApp } from 'firebase/app'; 
+// import { getAnalytics } from "firebase/analytics";
+// const firebase = initializeApp();
+
+const auth = getAuth();
+const firestore = getFirestore();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcaOg7oEY5ZRGBg-Mz8-lR2SNrdWL7Y5c",
+  authDomain: "sjattapp.firebaseapp.com",
+  projectId: "sjattapp",
+  storageBucket: "sjattapp.appspot.com",
+  messagingSenderId: "88197017781",
+  appId: "1:88197017781:web:af44c40944bb23c5290efc",
+  measurementId: "G-TM0WZG3KH0"
+};
+
+firebase.initializeApp({
+  firebaseConfig
+})
+
+
 
 function App() {
-  return null
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className='App'>
+      <header>
+
+        
+      </header>
+
+      <section>
+        { user ? <ChatRoom /> : <SignIn /> }
+      </section>
+    </div>
+  )
 }
 
 export default App;
